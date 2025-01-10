6cd2fdafb269e64a5dd1fe66c919f8f5000e166c
Michael Dinerstein <md@missionmike.dev>
2025-01-09 23:10:51 -0800

    feat: fullscreen note.

--- src/components/Instructions.tsx
+++ src/components/Instructions.tsx
@@ -21,6 +21,9 @@ export const Instructions = () => {
     <div className="instructions" data-has-typed={hasTyped}>
       <h1>SmashKeys</h1>
       <p>Type to make stuff happen! 🎉</p>
+      <p>
+        <em>(best in full-screen!)</em>
+      </p>
     </div>
   );
 };
